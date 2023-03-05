9406e29a4b367b0015f8c0abaeaa9c08199d60cf
Jordi Ribellas <ribellas2002@gmail.com>
2023-03-05 12:03:32 +0100

    primer commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+	public static void menu() {
+		
+		System.out.println("##########\nMENU\n##########");
+		
+	}
+}
